48b2a72e889cc3819b4769b27d5f09c7a6ef4f86
Piotr Pawlowski <pkpawlowski@gmail.com>
2024-08-24 22:05:44 +0200

    code unused

--- media/lua/shared/BanditBrain.lua
+++ media/lua/shared/BanditBrain.lua
@@ -5,52 +5,6 @@ function BanditBrain.Get(zombie)
     return modData.brain
 end
 
-function BanditBrain.Create(zombie, master)
-    local brain = {}
-    brain.id = BanditUtils.GetCharacterID(zombie)
-    brain.master = BanditUtils.GetCharacterID(master)
-    brain.clan = 0
-    brain.fullname = "Unknown"
-    brain.hostile = true
-    brain.sleeping = false
-    brain.enslaved = true
-    brain.combat = false
-    brain.firing = false
-    brain.endurance = 1.00
-    brain.speech = 0.00
-
-    brain.capabilities = {}
-    brain.inventory = {}
-    brain.world = {}
-    brain.tasks = {}
-
-    brain.weapons = {}
-    brain.weapons.melee = false
-    brain.weapons.primary = {}
-    brain.weapons.primary.name = false
-    brain.weapons.primary.magSize = 20
-    brain.weapons.primary.bulletsLeft = 0
-    brain.weapons.primary.magCount = 0
-    brain.weapons.secondary = {}
-    brain.weapons.secondary.name = false
-    brain.weapons.secondary.magSize = 20
-    brain.weapons.secondary.bulletsLeft = 0
-    brain.weapons.secondary.magCount = 0
-
-    brain.program = {}
-    brain.program.role = nil
-    brain.program.stage = nil
-
-    brain.target = {}
-    brain.target.x = nil
-    brain.target.y = nil
-    brain.target.z = nil
-
-    local modData = zombie:getModData()
-    modData.brain = brain
-    return brain
-end
-
 function BanditBrain.Update(zombie, brain)
     local modData = zombie:getModData()
     modData.brain = brain
